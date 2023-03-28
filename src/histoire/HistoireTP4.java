05e8cb3e40c3858a2d6506869a9fb6e58ee61313
alyssia leclerc <ninaly.leclerc@gmail.com>
2023-03-28 16:46:47 +0200

    création de la classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,16 @@
+package histoire;
+import javax.sound.sampled.Port;
+
+import personnages.Humain;
+
+public class HistoireTP4 {
+	
+public static void main(String[] args) {
+	Humain Prof = new Humain("Prof", "kombucha", 54);
+	Prof.direBonjour();
+	Prof.acheter("une boisson", 12);
+	Prof.boire();
+	Prof.acheter("un jeu", 2);
+	Prof.acheter("un kimono", 50);
+}
+}
