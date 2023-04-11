69679bd30d0ac3e1e317e24920c8f351b3871c4c
alyssia leclerc <ninaly.leclerc@gmail.com>
2023-04-11 19:43:43 +0200

    les humain ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,16 @@
 package personnages;
 
+import java.util.Iterator;
+import java.util.concurrent.BrokenBarrierException;
+
 public class Humain {
+	private static final int  TAILLEMAX = 30;
+	
 	private String nom;
 	private String boisson;
 	protected int argent;
+	protected Humain [] memoire = new Humain[TAILLEMAX];
+	protected int nbConnaissance = 0;
 	
 	public Humain(String nom, String boisson, int argent) {
 		this.nom = nom;
@@ -48,5 +55,39 @@ public class Humain {
 			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
 		}
 	}
+	
+	private void memoriser(Humain humain) {
+		if (nbConnaissance == TAILLEMAX) {
+			for (int i = 1; i < nbConnaissance; i++) {
+				memoire[i-1] = memoire[i];
+			}
+			memoire[nbConnaissance-1] = humain;
+		}
+		else {
+			memoire[nbConnaissance] = humain;
+			nbConnaissance++;
+		}
+	}
+	
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+	
+	public void faireConnaissanceAvec (Humain humain) {
+		direBonjour();
+		humain.repondre(this);
+		memoriser(humain);
+	}
+	
+	public void listerConnaissance() {
+		String texte = "Je connais beaucooup de monde dont: ";
+		for (int i = 0; i < nbConnaissance; i++) {
+			texte += memoire[i].getNom();
+			if (i != nbConnaissance-1) texte += ", ";
+		}
+		parler(texte);
+	}
+	
 	
 }
